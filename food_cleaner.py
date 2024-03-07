5e18af68d29c937c6f9e531492d5694136fe115a
Camila Figueira <60558085+ConfettiRaindrop@users.noreply.github.com>
2024-03-07 21:35:11 +0000

    remove uneeded code

--- food_cleaner.py
+++ food_cleaner.py
@@ -6,12 +6,10 @@ for food in range(len(foods) - 1):
     if "noun" in current_food:
         current_food = current_food[:current_food.find("noun")]
         current_food.strip()
-        #foodlist.append(current_food)
         backend_foodlist.write(current_food + "\n")
     else:
         foods.remove(current_food)
         food -= 1
     print(food)
 
-#backend_foodlist.writelines(foodlist)
 backend_foodlist.close()
