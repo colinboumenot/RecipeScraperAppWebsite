a046fd32e64383872c4a0bff92ec02114480b69c
Sofiya Chubich <95042138+SofiyaChubich@users.noreply.github.com>
2024-03-12 13:05:24 -0700

    Update database.py

---
+++ database.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+class SQL:
+
+    def __init__(self) -> None:
+        #add in the database
+        pass
+
+    def add(self):
+        pass
+
+    def insert(self, column, value):
+        pass
+
+    def delete(self, row):
+        pass
+
+    def update(self, column, value):
+        pass
+
+    def select(self, column, value):
+        pass
