692da6d1d2b68c0b0d34916dd33003d200d3d7b6
Sindhura-Satti <s-ssatti@lwsd.org>
2024-03-19 12:37:17 -0700

    connecting to mysql

--- database.py
+++ database.py
@@ -1,13 +1,27 @@
 import sqlite3
+import mysql.connector
 
 class SQL:
+    
 
-    def __init__(self) -> None:
+    mydb = mysql.connector.connect(
+        host = "localhost:3306",
+        user="root",
+        password="Sr1k@r36",
+        database="orders"
+
+    )
+    cursor = mydb.cursor()
+    sql = "INSERT INTO Pets (petID, petName, kind, gender, age, ownerID, adoptionID) VALUES(R3-7551, 'Keller', 'Parrot', 'female', '2', 7908, NULL);"
+    cursor = mydb.cursor(sql)
+
+    def __init__(self, petID = 0, petName = 0, kind = 0, gender = 0, age = 0, ownerID = 0, adoptionID = 0) -> None:
         #add in the database
-        pass
+        sql = "USE orders; INSERT INTO Pets (petID, petName, kind, gender, age, ownerID, adoptionID) VALUES(R3-7551, 'Keller', 'Parrot', 'female', '2', 7908, NULL);"
+        
 
     def add(self):
-        pass
+        sql = "INSERT INTO Pets (petID, petName, kind, gender, age, ownerID, adoptionID) VALUES(R3-7551, 'Keller', 'Parrot', 'female', '2', 7908, NULL);"
 
     def insert(self, column, value):
         pass
