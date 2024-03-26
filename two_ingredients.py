461f359e8b75bba117959c453805a64500fa7ed2
boumenotcolin <90056323+boumenotcolin@users.noreply.github.com>
2024-03-26 20:44:09 +0000

    #18 checked all recipes for compound word ingredients

--- two_ingredients.py
+++ two_ingredients.py
@@ -1,12 +1,14 @@
 from recipe import Recipe
 import pickle
 import inflect 
+import re
 
-with open('ScrapedRecipes/all_recipes_3.pickle', 'rb') as f:
+with open('ScrapedRecipes/all_recipes.pickle', 'rb') as f:
     recipes = pickle.load(f)
 
 temp_recipes = recipes
 recipes = []
+new_ingredients = set()
 
 for item in temp_recipes:
     if item is not None:
@@ -14,7 +16,8 @@ for item in temp_recipes:
 
 foods = set(x.strip().lower() for x in open('raw_data/backend_food_names.txt', 'r+').readlines())
 
-edge_cases = ['watercress', 'delicious', 'cress', 'peppercress', 'guinness', 'bass', 'christmas', 'grits', 'lotus', 'seabass', 'angus', 'cablres', 'ras', 'hummus', 'skinless', "'s", 'octopus', 'pastis', 'hibiscus', 'molasses', 'lemongrass', 'couscous', 'cactus', 'citrus', 'bitters', 'swiss', 'gras', 'wheatgrass', 'moss', 'jus']
+## Words that look plural but are really singular, brute force approach but still works
+edge_cases = ['watercress', 'delicious', 'cress', 'peppercress', 'guinness', 'bass', 'christmas', 'grits', 'lotus', 'seabass', 'angus', 'cablres', 'ras', 'hummus', 'fries', 'skinless', "'s", 'octopus', 'pastis', 'hibiscus', 'molasses', 'lemongrass', 'couscous', 'cactus', 'citrus', 'bitters', 'swiss', 'gras', 'wheatgrass', 'moss', 'jus', 'as']
 
 ## Convert all plural nouns to singular, reduces ingredients that need to be entered into food names
 def plural_to_singular(ingredients):
@@ -38,10 +41,57 @@ def plural_to_singular(ingredients):
         new_ingredients.append(" ".join(singular_phrase).lower())
     return new_ingredients
 
+## Takes string that has more ingredients than it should, and finds the individual ingredients oftentimes these ingredients are compounds
+## Ex "pork shoulder" should be 1 ingredient, before this function 'pork' and 'shoulder' were two seperate ingredients
+def build_new_ingredient(ingredient, matches, start):
+    new_ingredient = ''
+    found_items = []
+
+    for x in range(start, len(ingredient)):
+        if ingredient[x] in matches:
+            new_ingredient += ingredient[x] + ' '
+            found_items.append(ingredient[x])
+        else:
+            break
+    
+    for x in found_items:
+        if x in matches:
+            matches.remove(x)
+    
+    if len(matches) > 0:
+        new_start = min(ingredient.index(word) for word in matches)
+        return new_start, matches, new_ingredient.strip()
+    else:
+        return -1, matches, new_ingredient.strip()
+
+
+## Called if too many ingredients detected
+def multiple_ingredient_error(ingredient, matches):
+    ## print(f"{ingredient} matches: {matches}")
+    ingredient = ingredient.split(' ')
+    ingredients = []
+    edited_matches = []
+    for x in matches:
+        x = x.split(' ')
+        for word in x:
+            edited_matches.append(word)
+    start = min(ingredient.index(word) for word in edited_matches)
+    
+    while len(edited_matches) > 0:
+        start, edited_matches, new_ingredient = build_new_ingredient(ingredient, edited_matches, start)
+        if new_ingredient != '':
+            ingredients.append(new_ingredient)
+            if new_ingredient not in foods:
+                new_ingredients.add(new_ingredient)
+
+    ## print(ingredients)
+
 
 def get_ingredients(recipe):
     matches = []
     for ingredient in recipe.ingredients:
+        ## Get rid of contents inside parentheses, useful for determining units later on, but not for ingredients
+        ingredient = re.sub("[\(\[].*?[\)\]]", "", ingredient)
         temp = []
         duplicates = []
         for food in foods:
@@ -78,21 +128,29 @@ def get_ingredients(recipe):
             if 'cloves' in temp:
                 temp.remove('cloves')
 
-        if len(temp) == 0:
-            if 'water' in set(ingredient.split(' ')) or 'ice' in set(ingredient.split(' ')) or 'cooking spray' in ingredient:
-                continue
-            elif 'recipe follow' in ingredient:
-                continue
-            else:
+        ## if len(temp) == 0:
+            ## if 'water' in set(ingredient.split(' ')) or 'ice' in set(ingredient.split(' ')) or 'cooking spray' in ingredient:
+                ## continue
+            ## elif 'recipe follow' in ingredient:
+                ## continue
+            ## else:
                 ## with open('raw_data/unknown_ingredients.txt', 'a') as f:
                     ## f.write('no ingredient ' + ingredient + '\n')
-                pass
+                ## pass
         ##if len(temp) == 1 and ('and' in ingredient or 'or' in ingredient):
             ##with open('raw_data/unknown_ingredients.txt', 'a') as f:
                 ##f.write('and or ingredient ' + ingredient + ' ' + ' '.join(temp) + '\n')
-        ##if len(temp) > 1:
-            ##with open('raw_data/unknown_ingredients.txt', 'a') as f:
-                ##f.write('multiple ingredient ' + ingredient + ' ' + ' '.join(temp) + '\n')
+        if (len(temp) > 1):
+            if 'or' in ingredient.split(' '):
+                continue
+            elif 'and' in ingredient.split(' '):
+                continue
+            elif 'such as' in ingredient or 'recipe follow' in ingredient or 'optional' in ingredient:
+                continue
+            else:
+                multiple_ingredient_error(ingredient, temp)
+                with open('raw_data/unknown_ingredients.txt', 'a') as f:
+                    f.write('multiple ingredient ' + ingredient + ' ' + ' '.join(temp) + '\n')
         for x in temp:
             matches.append(x)
     return matches
@@ -102,11 +160,10 @@ def get_ingredients(recipe):
 
 print(len(recipes))
 
-for x in range(10):
+for x in range(20000, len(recipes)):
     recipe = recipes[x]
     recipe.ingredients = plural_to_singular(recipe.ingredients)
-    print(recipe.ingredients)
-    print(get_ingredients(recipe))
+    get_ingredients(recipe)
 
 ## Helper method to sort ingredient file alphabetically
 ## def sort_file(input_file, output_file):
@@ -115,3 +172,7 @@ for x in range(10):
             ## o.write("\n".join(sorted(f.read().splitlines())))
 
 ## sort_file("raw_data/backend_food_names.txt", "raw_data/food_names.txt")
+
+with open('raw_data/new_ingredients.txt', 'w+') as f:
+    for x in new_ingredients:
+        f.write(x + '\n')
