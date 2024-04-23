fc2fa619bd19fa36229e8ee885ccf22ba9861c48
nikilnam <awesomenikil3@gmail.com>
2024-04-22 17:56:36 -0700

    Delete Button Added

--- UI/homepage.py
+++ UI/homepage.py
@@ -35,14 +35,15 @@ title, button_filter, button_recipe = create_home_screen()
 current_screen = 'home'
 input_box = search_results = back_button = filter_title = recipe_title = items_text_box = None
 item_list = []
+delete_buttons = []
 
 # Handling screens and transitions
 def draw_filter_search_screen():
-    global input_box, search_results, back_button, filter_title, items_text_box, current_screen
+    global input_box, search_results, back_button, filter_title, items_text_box, current_screen, delete_buttons
     title.hide()
     button_filter.hide()
     button_recipe.hide()
-    
+
     filter_title = pygame_gui.elements.UILabel(relative_rect=pygame.Rect(200, 50, 400, 50),
                                                text='List out your ingredients',
                                                manager=manager)
@@ -54,32 +55,41 @@ def draw_filter_search_screen():
     search_results = pygame_gui.elements.UIPanel(relative_rect=pygame.Rect(50, 160, 700, 300),
                                                  manager=manager)
     items_text_box = pygame_gui.elements.UITextBox(html_text='',
-                                                   relative_rect=pygame.Rect(0, 0, 690, 290),
+                                                   relative_rect=pygame.Rect(0, 0, 640, 290),
                                                    manager=manager,
                                                    container=search_results)
     input_box.focus()
     current_screen = 'filter_search'
+    update_items_display()
 
 def update_items_display():
-    formatted_text = '<br>'.join(f'{item}' for item in item_list)  # Ensure correct HTML formatting
+    global delete_buttons
+    formatted_text = '<br>'.join(f'{item}' for item in item_list)
     items_text_box.html_text = formatted_text
-    items_text_box.rebuild()  # Rebuild the text box to update the display
+    items_text_box.rebuild()
 
-def draw_recipe_search_screen():
-    global input_box, back_button, recipe_title, current_screen
-    title.hide()
-    button_filter.hide()
-    button_recipe.hide()
-    
-    recipe_title = pygame_gui.elements.UILabel(relative_rect=pygame.Rect(200, 50, 400, 50),
-                                               text='Search Your Recipe',
-                                               manager=manager)
-    back_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(50, 550, 100, 40),
-                                               text='Back',
-                                               manager=manager)
-    input_box = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(50, 100, 700, 50),
-                                                    manager=manager)
-    current_screen = 'recipe_search'
+    # Remove old buttons
+    for btn in delete_buttons:
+        btn.kill()
+    delete_buttons = []
+
+    # Add new delete buttons
+    y_offset = 0
+    for idx, item in enumerate(item_list):
+        btn = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(650, 10 + y_offset, 30, 30),
+                                           text='Del',
+                                           manager=manager,
+                                           container=search_results,
+                                           object_id=f'delete_{idx}')
+        btn.item_index = idx  # Custom attribute to identify the button
+        delete_buttons.append(btn)
+        y_offset += 40  # Adjust y offset for next button
+
+def delete_item(button):
+    global item_list
+    if button.item_index < len(item_list):
+        item_list.pop(button.item_index)
+        update_items_display()
 
 def handle_ui_events(event):
     global current_screen, item_list
@@ -91,21 +101,23 @@ def handle_ui_events(event):
                 draw_filter_search_screen()
             elif event.ui_element == button_recipe:
                 draw_recipe_search_screen()
-            elif back_button and event.ui_element == back_button and current_screen != 'home':
+            elif back_button and event.ui_element == back_button:
                 title.show()
                 button_filter.show()
                 button_recipe.show()
-                if back_button:
-                    back_button.hide()
-                if input_box:
-                    input_box.hide()
-                if filter_title:
-                    filter_title.hide()
+                back_button.hide()
+                input_box.hide()
+                filter_title.hide()
                 if recipe_title:
                     recipe_title.hide()
-                if search_results:
-                    search_results.hide()
+                search_results.hide()
                 current_screen = 'home'
+            else:
+    
+                for btn in delete_buttons:
+                    if event.ui_element == btn:
+                        delete_item(btn)
+                        break
     elif event.type == pygame.KEYDOWN:
         if event.key == pygame.K_RETURN and current_screen == 'filter_search' and input_box.get_text():
             item_list.append(input_box.get_text())
@@ -114,7 +126,7 @@ def handle_ui_events(event):
 
 # Main loop
 clock = pygame.time.Clock()
-running = True  
+running = True
 
 while running:
     time_delta = clock.tick(60) / 1000.0
